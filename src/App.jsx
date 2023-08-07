beafadeb4ca0d4274ddf9579afdcf6bdeb7cf957
mjamecny <jamecny.m@tutanota.com>
2023-08-07 22:27:20 +0200

    how to not fetch data in react

--- src/App.jsx
+++ src/App.jsx
@@ -50,9 +50,16 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0)
 
+const KEY = "b2453ff5"
+
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData)
-  const [watched, setWatched] = useState(tempWatchedData)
+  const [movies, setMovies] = useState([])
+  const [watched, setWatched] = useState([])
+
+  // fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+  // fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=interstellar`)
+  //   .then((res) => res.json())
+  //   .then((data) => setMovies(data.Search))
 
   return (
     <>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import ReactDOM from "react-dom/client"
-// import App from './App.jsx'
-// import './index.css'
+import App from "./App.jsx"
+import "./index.css"
 
 import StarRating from "./StarRating"
 
@@ -18,12 +18,12 @@ function Test() {
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating size={24} color="red" className="test" defaultRating={3} />
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 )
