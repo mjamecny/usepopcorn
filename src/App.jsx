53def78ab86d888365e15816c2a714e273920cd9
mjamecny <jamecny.m@tutanota.com>
2023-08-08 18:20:00 +0200

    initializing state with callback (lazy initial state)

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,16 @@ const KEY = "b2453ff5"
 
 export default function App() {
   const [movies, setMovies] = useState([])
-  const [watched, setWatched] = useState([])
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState("")
   const [query, setQuery] = useState("")
   const [selectedId, setSelectedId] = useState(null)
+
+  // const [watched, setWatched] = useState([])
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem("watched")
+    return JSON.parse(storedValue)
+  })
 
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id))
@@ -24,11 +29,19 @@ export default function App() {
 
   function handleAddWatched(movie) {
     setWatched((watched) => [...watched, movie])
+    // localStorage.setItem("watched", JSON.stringify([...watched, movie]))
   }
 
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id))
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched))
+    },
+    [watched]
+  )
 
   useEffect(
     function () {
