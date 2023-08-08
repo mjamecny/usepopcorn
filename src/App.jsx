1fe49d9d1d39bb7e6dce3010a7cffbb92ca5e588
mjamecny <jamecny.m@tutanota.com>
2023-08-08 19:43:29 +0200

    refs to select dom elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import StarRating from "./StarRating"
 
 const average = (arr) =>
@@ -170,11 +170,26 @@ function NumResults({ movies }) {
 }
 
 function Search({ query, setQuery }) {
-  useEffect(function () {
-    const el = document.querySelector(".search")
-    console.log(el)
-    el.focus()
-  }, [])
+  const inputEl = useRef(null)
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return
+
+        if (e.code === "Enter") {
+          inputEl.current.focus()
+          setQuery("")
+        }
+      }
+
+      document.addEventListener("keydown", callback)
+
+      return () => document.removeEventListener("keydown", callback)
+    },
+    [setQuery]
+  )
+
   return (
     <input
       className="search"
@@ -182,6 +197,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   )
 }
