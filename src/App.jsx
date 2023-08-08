9011d848bed1e5d343778c89409ca964ae3d7967
mjamecny <jamecny.m@tutanota.com>
2023-08-08 20:22:59 +0200

    refs to persist data between renders

--- src/App.jsx
+++ src/App.jsx
@@ -273,6 +273,15 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [isLoading, setIsLoading] = useState(false)
   const [userRating, setUserRating] = useState("")
 
+  const countRef = useRef(0)
+
+  useEffect(
+    function () {
+      if (userRating) countRef.current = countRef.current + 1
+    },
+    [userRating]
+  )
+
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId)
   const watchedUserRating = watched.find(
     (movie) => movie.imdbID === selectedId
@@ -307,6 +316,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split(" ").at(0)),
       userRating,
+      countRatingDecisions: countRef.current,
     }
     onAddWatched(newWatchedMovie)
     onCloseMovie()
