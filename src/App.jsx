6d59bc091055a17c575ebee95552cbebf92c205d
mjamecny <jamecny.m@tutanota.com>
2023-08-08 11:10:47 +0200

    useeffect to the rescue

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const tempMovieData = [
   {
@@ -56,10 +56,11 @@ export default function App() {
   const [movies, setMovies] = useState([])
   const [watched, setWatched] = useState([])
 
-  // fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-  // fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=interstellar`)
-  //   .then((res) => res.json())
-  //   .then((data) => setMovies(data.Search))
+  useEffect(() => {
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=interstellar`)
+      .then((res) => res.json())
+      .then((data) => setMovies(data.Search))
+  }, [])
 
   return (
     <>
