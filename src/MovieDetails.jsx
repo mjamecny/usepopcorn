b7d10d8b51c9874162c126361caed303796754a1
mjamecny <jamecny.m@tutanota.com>
2023-08-09 12:24:44 +0200

    edit api url

--- src/MovieDetails.jsx
+++ src/MovieDetails.jsx
@@ -66,7 +66,7 @@ export default function MovieDetails({
       async function getMovieDetails() {
         setIsLoading(true)
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         )
         const data = await res.json()
         setMovie(data)
